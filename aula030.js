68b88dc1d113cecfa069549b9dd21c77e0251052
MarcioKruger <ms.kruger@hotmail.com>
2023-09-12 08:49:45 -0300

    .

---
+++ aula030.js
@@ -0,0 +1,16 @@
+const d1 = document.getElementById("d1")
+const d2 = document.getElementById("d2")
+const d3 = document.getElementById("d3")
+const d4 = document.getElementById("d4")
+const d5 = document.getElementById("d5")
+const d6 = document.getElementById("d6")
+
+
+const darray=[d1,d2,d3,d4,d5,d6]
+//for(e of darray)
+//    e.innerHTML='Marcio'
+
+darray.map((i)=>{
+    i.innerHTML="kruger"
+
+})
---
+++ aula031.js
@@ -0,0 +1,5 @@
+const d1 = [...document.getElementsByTagName('div')]
+console.log(d1)
+d1.map((e)=>{
+    console.log(e)
+})
